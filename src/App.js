194f7f98c9fe1ffa69de556be65ec818d30f44dc
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2023-08-27 04:21:18 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import { Canvas } from '@react-three/fiber'
-import { Center, AccumulativeShadows, RandomizedLight, Environment, OrbitControls } from '@react-three/drei'
-import { useControls, button } from 'leva'
-import { Model } from './Car'
-import { Effects } from './Effects'
+import { Canvas } from '@react-three/fiber';
+import { Center, AccumulativeShadows, RandomizedLight, Environment, OrbitControls } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
+import { tweakPane } from './tweakpane.js';
+import { Model } from './Car';
+import { Effects } from './Effects';
 
 export default function App() {
-  const { color, realism, importanceSampling } = useControls({
+  const pane = useRef(null);
+  const params = useRef({
     color: '#ff9621',
     realism: true,
     importanceSampling: true,
-    screenshot: button(() => {
-      const link = document.createElement('a')
-      link.setAttribute('download', 'canvas.png')
-      link.setAttribute('href', document.querySelector('canvas').toDataURL('image/png').replace('image/png', 'image/octet-stream'))
-      link.click()
-    })
-  })
+  });
+
+  useEffect(() => {
+    pane.current = new tweakPane();
+    pane.current.addInput(params.current, 'color');
+    pane.current.addInput(params.current, 'realism');
+    pane.current.addInput(params.current, 'importanceSampling');
+    pane.current.addButton({
+      title: 'Screenshot',
+    }).on('click', () => {
+      const link = document.createElement('a');
+      link.setAttribute('download', 'canvas.png');
+      link.setAttribute('href', document.querySelector('canvas').toDataURL('image/png').replace('image/png', 'image/octet-stream'));
+      link.click();
+    });
+  }, []);
+
   return (
     <Canvas gl={{ antialias: false, preserveDrawingBuffer: true }} shadows camera={{ position: [4, 0, 6], fov: 35 }}>
       <group position={[0, -0.75, 0]}>
         <Center top>
-          <Model color={color} />
+          <Model color={params.current.color} />
         </Center>
         <AccumulativeShadows>
           <RandomizedLight position={[2, 5, 5]} />
         </AccumulativeShadows>
       </group>
       <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
-      {realism && <Effects importanceSampling={importanceSampling} />}
+      {params.current.realism && <Effects importanceSampling={params.current.importanceSampling} />}
       <Environment preset="dawn" background blur={1} />
     </Canvas>
-  )
+  );
 }
